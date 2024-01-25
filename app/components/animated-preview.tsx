4b2ffdef600a117dcaa5d85af13cddc8398f8bcb
Rajesh Babu <rajeshdavid.b@gmail.com>
2024-01-25 10:30:01 +0100

    Remove some items that I cannot control

--- app/components/animated-preview.tsx
+++ app/components/animated-preview.tsx
@@ -46,7 +46,7 @@ const people = [
 
 export function AnimatedTooltipPreview() {
   return (
-    <div className="flex flex-row items-center justify-center mb-10 w-full">
+    <div className="flex flex-row items-center justify-center mt-10 w-full">
       <AnimatedTooltip items={people} />
     </div>
   );
--- app/components/hero-3d-card.tsx
+++ app/components/hero-3d-card.tsx
@@ -2,7 +2,7 @@ import { CardBody, CardContainer, CardItem } from "./ui/3d-card";
 
 export function Hero3DCard() {
   return (
-    <CardContainer className="inter-var">
+    <CardContainer className="inter-var px-4">
       <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
         <CardItem
           translateZ="50"
